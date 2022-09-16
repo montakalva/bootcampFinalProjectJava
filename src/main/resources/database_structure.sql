9770c9bcd6caa1b55760d2057a7d950c3a604d67
montakalva <montamona@inbox.lv>
2022-09-16 21:56:15 +0300

    "SQL Structure update"

--- src/main/resources/database_structure.sql
+++ src/main/resources/database_structure.sql
@@ -22,3 +22,87 @@ phoneNumber int not null,
 PRIMARY KEY(userID),
 FOREIGN KEY(apartmentID) REFERENCES apartments(apartmentID)
 );
+
+CREATE TABLE IF NOT EXISTS waterMeasurements(
+measurementID int not null AUTO_INCREMENT,
+coldWaterMeasurementCurrent DOUBLE not null,
+coldWaterConsumption DOUBLE not null,
+hotWaterMeasurementCurrent DOUBLE not null,
+hotWaterConsumption DOUBLE not null,
+submitAt TIMESTAMP default current_timestamp,
+userID int,
+apartmentNo int,
+PRIMARY KEY(measurementID),
+FOREIGN KEY(userID) REFERENCES users(userID),
+FOREIGN KEY(apartmentNo) REFERENCES apartments(apartmentID)
+);
+
+CREATE TABLE IF NOT EXISTS voting(
+votingID int not null AUTO_INCREMENT,
+votingTitle VARCHAR (255) not null,
+votingAnswer VARCHAR (100),
+votingStatus VARCHAR (100),
+votingAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+userID int,
+apartmentNo int,
+PRIMARY KEY(votingID),
+FOREIGN KEY(userID) REFERENCES users(userID),
+FOREIGN KEY(apartmentNo) REFERENCES apartments (apartmentID)
+);
+
+CREATE TABLE IF NOT EXISTS messages(
+messageID int not null AUTO_INCREMENT,
+messageTitle VARCHAR (500),
+messageStatus VARCHAR (100),
+messageComment VARCHAR (700),
+commentOnMessageID int,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+userID int,
+apartmentNo int,
+PRIMARY KEY(messageID),
+FOREIGN KEY(userID) REFERENCES users(userID),
+FOREIGN KEY(apartmentNo) REFERENCES apartments (apartmentID)
+);
+
+CREATE TABLE IF NOT EXISTS invoices(
+invoiceID int not null AUTO_INCREMENT,
+invoiceNo VARCHAR (100),
+invoiceTitle VARCHAR (255),
+invoiceCompany VARCHAR (255),
+invoiceIssueDate VARCHAR (100),
+invoiceDescription VARCHAR (500),
+invoiceSubTotal DOUBLE,
+invoiceTax DOUBLE,
+invoiceTotalAmount DOUBLE,
+invoiceStatus VARCHAR (100),
+invoicePaidOn VARCHAR (100),
+PRIMARY KEY(invoiceID)
+);
+
+INSERT INTO users (userType, firstName, lastName, password, email, phoneNumber) VALUES ("MANAGER","John", "Smith", "john", "john@gmail.com", 555);
+
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (1, 29.00, 1, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (1, 29.00, 1, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (2, 29.00, 1, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (3, 29.00, 1, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (4, 29.00, 2, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (5, 29.00, 2, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (6, 29.00, 2, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (7, 29.00, 3, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (8, 29.00, 3, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (9, 29.00, 3, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (10, 29.00, 4, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (11, 29.00, 4, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (12, 29.00, 4, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (13, 29.00, 5, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (14, 29.00, 5, 2);
+INSERT INTO apartments (apartmentNo, sqm, floorNo, roomCount) VALUES (15, 29.00, 5, 2);
+
+INSERT INTO users (apartmentNo, userType, firstName, lastName, password, email, phoneNumber) VALUES (1, "OWNER","Peter", "White", "peter", "peter@gmail.com", 444);
+
+SELECT * FROM users;
+SELECT * FROM apartments;
+SELECT * FROM waterMeasurements;
+SELECT * FROM voting;
+SELECT * FROM messages;
+SELECT * FROM invoices;
